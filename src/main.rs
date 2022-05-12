df344fea348b013c970f4fdd026995defc9f0699
Brynley Llewellyn-Roux <blynblack@gmail.com>
2022-05-13 09:46:52 +1000

    Removed useless spaces

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 // chess - A Rust implementation of the famous game Chess.
-// Copyright (C) 2022 Brynley Llewellyn-Roux and Aryan Jassal
-// 
+// Copyright (C) 2022  Brynley Llewellyn-Roux and Aryan Jassal
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
