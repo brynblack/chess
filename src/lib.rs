cb31dc4a558b69fe259e5d4f370193b447c01219
Brynley Llewellyn-Roux <blynblack@gmail.com>
2022-05-23 12:11:30 +1000

    Created skeleton for the State struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,35 @@
+use winit::window::Window;
+
+struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+}
+
+impl State {
+    async fn new(window: &Window) -> Self {
+        todo!()
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        todo!()
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        todo!()
+    }
+
+    fn update(&mut self) {
+        todo!()
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        todo!()
+    }
+}
+
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
