0ef55d7e0ab7b04e43eb2a1452e44f9e9d51aae7
Aryan Jassal <aryanjassal.au@gmail.com>
2022-05-30 19:46:17 +1000

    changed indices rendering to render an octagon with colors

--- src/lib.rs
+++ src/lib.rs
@@ -17,9 +17,6 @@ struct State {
     num_indices: u32,
 }
 
-// unsafe impl bytemuck::Pod for Vertex {}
-// unsafe impl bytemuck::Zeroable for Vertex {}
-
 #[repr(C)]
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 struct Vertex {
@@ -64,29 +61,24 @@ impl Vertex {
 }
 
 const VERTICES: &[Vertex] = &[
-    Vertex {
-        position: [-0.0868241, 0.49240386, 0.0],
-        color: [0.5, 0.0, 0.5],
-    }, // A
-    Vertex {
-        position: [-0.49513406, 0.06958647, 0.0],
-        color: [0.5, 0.0, 0.5],
-    }, // B
-    Vertex {
-        position: [-0.21918549, -0.44939706, 0.0],
-        color: [0.5, 0.0, 0.5],
-    }, // C
-    Vertex {
-        position: [0.35966998, -0.3473291, 0.0],
-        color: [0.5, 0.0, 0.5],
-    }, // D
-    Vertex {
-        position: [0.44147372, 0.2347359, 0.0],
-        color: [0.5, 0.0, 0.5],
-    }, // E
+    Vertex { position: [-0.3, 0.9, 0.0], color: [1.0, 0.0, 0.0], }, // A
+    Vertex { position: [-0.6, 0.45, 0.0], color: [0.0, 1.0, 0.0], }, // B
+    Vertex { position: [-0.6, -0.45, 0.0], color: [0.0, 0.0, 1.0], }, // C
+    Vertex { position: [-0.3, -0.9, 0.0], color: [1.0, 1.0, 0.0], }, // D
+    Vertex { position: [0.3, -0.9, 0.0], color: [0.0, 1.0, 1.0], }, // E
+    Vertex { position: [0.6, -0.45, 0.0], color: [1.0, 1.0, 1.0], }, // F
+    Vertex { position: [0.6, 0.45, 0.0], color: [1.0, 0.0, 1.0], }, // G
+    Vertex { position: [0.3, 0.9, 0.0], color: [0.0, 0.0, 0.0], }, // H
 ];
 
-const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
+const INDICES: &[u16] = &[
+    7, 0, 1,
+    1, 6, 7,
+    1, 2, 6,
+    2, 5, 6,
+    2, 3, 5,
+    3, 4, 5,
+];
 
 impl State {
     async fn new(window: &Window) -> Self {
