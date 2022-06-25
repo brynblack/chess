bcc1ad7f5da052d2ab2ee930891ad9700f21a3c3
brynblack <49110391+brynblack@users.noreply.github.com>
2022-06-25 20:13:03 +1000

    Added first few functions for handling data

--- src/main.rs
+++ src/main.rs
@@ -15,5 +15,117 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 fn main() {
-    println!("Hello, world!");
+    let mut board: [[Square; 8]; 8] = [
+        [
+            Square::Rook,
+            Square::Knight,
+            Square::Bishop,
+            Square::Queen,
+            Square::King,
+            Square::Bishop,
+            Square::Knight,
+            Square::Rook,
+        ],
+        [
+            Square::Pawn,
+            Square::Pawn,
+            Square::Pawn,
+            Square::Pawn,
+            Square::Pawn,
+            Square::Pawn,
+            Square::Pawn,
+            Square::Pawn,
+        ],
+        [
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+        ],
+        [
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+        ],
+        [
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+        ],
+        [
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+            Square::Empty,
+        ],
+        [
+            Square::Pawn,
+            Square::Pawn,
+            Square::Pawn,
+            Square::Pawn,
+            Square::Pawn,
+            Square::Pawn,
+            Square::Pawn,
+            Square::Pawn,
+        ],
+        [
+            Square::Rook,
+            Square::Knight,
+            Square::Bishop,
+            Square::Queen,
+            Square::King,
+            Square::Bishop,
+            Square::Knight,
+            Square::Rook,
+        ],
+    ];
+    println!("{:?}", board);
+    assert!(check_if_square_empty(&Coord(5, 5), &board))
+}
+
+struct Coord(u32, u32);
+
+#[derive(Debug)]
+enum Square {
+    Bishop,
+    Empty,
+    King,
+    Knight,
+    Pawn,
+    Rook,
+    Queen,
+}
+
+// PROBABLY NOT NEEDED
+fn check_if_square_empty(coord: &Coord, board: &[[Square; 8]; 8]) -> bool {
+    match board[coord.1 as usize][coord.0 as usize] {
+        Square::Empty => true,
+        _ => false,
+    }
+}
+
+// TODO
+fn move_piece(square: &Square) {
+    match square {
+        Square::Pawn => println!("test"),
+        _ => (),
+    }
 }
