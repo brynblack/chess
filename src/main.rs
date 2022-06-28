cac95a98a240d3bb9578c5b2946e97fbfe5a89b5
Brynley Llewellyn-Roux <blynblack@gmail.com>
2022-06-28 11:50:20 +1000

    Formatted code and moved some logic into match expression

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ fn initial_setup(mut commands: Commands, _asset_server: Res<AssetServer>) {
     // Create a new board with default layout
     let mut board = Board::new(Board::default());
 
-    // Move a piece
+    // Move a piece (For testing purposes only)
     board
         .move_piece(&Coord { x: 0, y: 6 }, &Coord { x: 0, y: 5 })
         .unwrap_or_else(|err| eprintln!("{}", err));
@@ -39,15 +39,18 @@ fn initial_setup(mut commands: Commands, _asset_server: Res<AssetServer>) {
     let temp_piece_size = 40.0;
 
     // Render the board
+    // TODO: Center the board on the screen
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    for i in 0..board.layout.len() {
-        for j in 0..board.layout.len() {
-            let square_colour = if (i + j) % 2 == 0 {
+    for row in 0..board.layout.len() {
+        for square in 0..board.layout.len() {
+            // Alternate the square colour
+            let square_colour = if (row + square) % 2 == 0 {
                 Color::rgb(0.46, 0.59, 0.34)
             } else {
                 Color::rgb(0.93, 0.93, 0.82)
             };
 
+            // Render each square
             commands.spawn_bundle(SpriteBundle {
                 sprite: Sprite {
                     color: square_colour,
@@ -55,27 +58,31 @@ fn initial_setup(mut commands: Commands, _asset_server: Res<AssetServer>) {
                     ..default()
                 },
                 transform: Transform::from_xyz(
-                    square_size * i as f32 - (square_size * 8.0),
-                    square_size * j as f32 - (0.5 * square_size * 8.0),
+                    square_size * row as f32 - (square_size * 8.0),
+                    square_size * square as f32 - (0.5 * square_size * 8.0),
                     0.0,
                 ),
                 ..default()
             });
 
-            if board.layout[j][i] != Square::Empty {
-                commands.spawn_bundle(SpriteBundle {
-                    sprite: Sprite {
-                        color: Color::rgb(0.0, 0.0, 0.0),
-                        custom_size: Some(Vec2::new(temp_piece_size, temp_piece_size)),
+            // Render each chess piece
+            match board.layout[square][row] {
+                Square::Empty => (),
+                _ => {
+                    commands.spawn_bundle(SpriteBundle {
+                        sprite: Sprite {
+                            color: Color::rgb(0.0, 0.0, 0.0),
+                            custom_size: Some(Vec2::new(temp_piece_size, temp_piece_size)),
+                            ..default()
+                        },
+                        transform: Transform::from_xyz(
+                            square_size * row as f32 - (square_size * 8.0),
+                            square_size * square as f32 - (0.5 * square_size * 8.0),
+                            1.0,
+                        ),
                         ..default()
-                    },
-                    transform: Transform::from_xyz(
-                        square_size * i as f32 - (square_size * 8.0),
-                        square_size * j as f32 - (0.5 * square_size * 8.0),
-                        1.0,
-                    ),
-                    ..default()
-                });
+                    });
+                }
             }
         }
     }
